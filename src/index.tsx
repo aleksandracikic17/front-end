c73432af7808578f3fa3461a12a3a729262b459c
aleksandracikic17 <aleksandra.cikic.17@singimail.rs>
2020-06-02 19:32:07 +0200

    install required libraries

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/popper.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/css/fontawesome.css'
 
 ReactDOM.render(
   <React.StrictMode>
